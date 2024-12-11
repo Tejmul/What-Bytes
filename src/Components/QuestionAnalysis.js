77698a9101f924443e1a75b9329d0a8cc0ff3007
Tejmul Movin <tejmulmovin@gmail.com>
2024-12-11 21:56:51 +0530

    removed unused imports

--- src/Components/QuestionAnalysis.js
+++ src/Components/QuestionAnalysis.js
@@ -1,5 +1,5 @@
 "use client"
-import React, { useState } from "react";
+import React from "react";
 import CircularProgressBar from "../Components/CircularProgress"
 
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
-import Image from "next/image";
 import Navbar from "../Components/Navbar"
 import SideBar from "../Components/SideBar"
-import StatsCard from "../Components/StatsCard"
-import SyllabusAnalysis from "../Components/SyllabusAnalysis"
 import DashBoard from "../Components/Dashboard"
 export default function Home() {
   return (
