391994effca256be653ca4b2c166beb78119056d
Tejmul Movin <tejmulmovin@gmail.com>
2024-12-11 22:19:28 +0530

    added configs

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    rules: {
+      '@next/next/no-img-element': 'off',
+      '@next/next/no-sync-scripts': 'off',
+    },
+    extends: ["next/core-web-vitals", "next/typescript"]
+  };
+  
